c173d83724b9605b167ba432f2ac71ea6fb8086f
MahesaSaftaLinggaS <mahesa67ls@gmail.com>
2025-07-11 19:39:35 +0700

    terakhir error pliss

--- resources/views/admin-layout.blade.php
+++ resources/views/admin-layout.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
-        @vite('resources/css/app.css')
-        @vite('resources/js/app.js')
+        <link rel="stylesheet" href="/build/assets/app-CuImFTX2.css">
+<script src="/build/assets/app-CbVi_I5T.js" defer></script>
     </head>
     <body>
 <!-- ========== MAIN CONTENT ========== -->
